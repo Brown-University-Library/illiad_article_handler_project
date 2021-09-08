d00be22929227a960b7b6a9b474553007f342b74
birkin <birkin.diana@gmail.com>
2021-09-08 17:04:52 -0400

    refactors user-status call, and create-user call, into one call.

--- illiad_article_handler_app/views.py
+++ illiad_article_handler_app/views.py
@@ -5,11 +5,44 @@ from django.conf import settings as project_settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from illiad_article_handler_app.lib import message_helper
+from illiad_article_handler_app.lib.illiad_helper import IlliadUserManager
 from illiad_article_handler_app.lib.shib_handler import Shibber
 from illiad_article_handler_app.lib.view_handler_helper import HandlerHelper
 
 
 log = logging.getLogger(__name__)
+
+
+# def handler(request):
+#     """ Creates ILLiad new-user if necessary, then redirects user to ILLiad form.
+#         On problem, stores error-message to session and redirects user to problem view. """
+#     log.debug( f'request.GET, ``{pprint.pformat(request.GET)}``' )
+#     ## setup ----------------------------------------------
+#     params_query_dict_copy = request.GET.copy()  # <https://stackoverflow.com/questions/5036498/django-rebuild-a-query-string-without-one-of-the-variables>
+#     log.debug( f'params_query_dict_copy, ``{pprint.pformat(params_query_dict_copy)}``' )
+#     assert type(params_query_dict_copy) == django.http.request.QueryDict, type(params_query_dict_copy)
+#     # request.session['error_message'] = ''
+#     ## check for new ILLiad user --------------------------
+#     shibber = Shibber()
+#     handler_helper = HandlerHelper()
+#     illiad_user_manager = IlliadUserManager()
+#     ( user_dct, err ) = shibber.prep_shib_dct( request.META, request.get_host() )
+#     if err:
+#         problem_response = handler_helper.create_problem_response( err ); return problem_response
+#     ( is_new_user, err ) = illiad_user_manager.check_new_user_status( user_dct['auth_id'] )
+#     if err:
+#         problem_response = handler_helper.create_problem_response( err, request ); return problem_response
+#     ## create new ILLiad user if necessary ----------------
+#     if is_new_user:
+#         ( create_result, err ) = new_user_helper.create_new_user( user_dct )
+#         if err:
+#             problem_response = handler_helper.create_problem_response( err, request ); return problem_response
+#     ## redirect to ILLiad form ----------------------------
+#     ( redirect_url, err ) = handler_helper.create_illiad_redirect_url( params_query_dict_copy )
+#     if err:
+#         problem_response = handler_helper.create_problem_response( err, request ); return problem_response
+#     else:
+#         return HttpResponseRedirect( redirect_url )
 
 
 def handler(request):
@@ -20,28 +53,22 @@ def handler(request):
     params_query_dict_copy = request.GET.copy()  # <https://stackoverflow.com/questions/5036498/django-rebuild-a-query-string-without-one-of-the-variables>
     log.debug( f'params_query_dict_copy, ``{pprint.pformat(params_query_dict_copy)}``' )
     assert type(params_query_dict_copy) == django.http.request.QueryDict, type(params_query_dict_copy)
-    # request.session['error_message'] = ''
-    ## check for new ILLiad user --------------------------
+    ## check for, and handle, new ILLiad user --------------------------
     shibber = Shibber()
     handler_helper = HandlerHelper()
-    ( good_shib_dct, err ) = shibber.prep_shib_dct( request.META, request.get_host() )
+    illiad_user_manager = IlliadUserManager()
+    ( user_dct, err ) = shibber.prep_shib_dct( request.META, request.get_host() )
     if err:
         problem_response = handler_helper.create_problem_response( err ); return problem_response
-    ( is_new_user, err ) = illiad_helper.check_new_user_status( shib_dct['eppn'] )
-    if err:
+    err = illiad_user_manager.manage_illiad_user_check( user_dct )
+    if err == 'problem with ILLiad status':  # note: if err is 'problem with ILLiad new-user check' -- flow continues
         problem_response = handler_helper.create_problem_response( err, request ); return problem_response
-    ## create new ILLiad user if necessary ----------------
-    if is_new_user:
-        ( create_result, err ) = new_user_helper.create_new_user( shib_dct )
-        if err:
-            problem_response = handler_helper.create_problem_response( err, request ); return problem_response
     ## redirect to ILLiad form ----------------------------
     ( redirect_url, err ) = handler_helper.create_illiad_redirect_url( params_query_dict_copy )
     if err:
         problem_response = handler_helper.create_problem_response( err, request ); return problem_response
     else:
         return HttpResponseRedirect( redirect_url )
-
 
 def message( request ):
     """ Shows user problem message. """
