07c16ea7c5743919054904312094e1e6181098af
birkin <birkin.diana@gmail.com>
2021-09-10 07:11:23 -0400

    gets illiad-check code working.

--- illiad_article_handler_app/lib/illiad_helper.py
+++ illiad_article_handler_app/lib/illiad_helper.py
@@ -15,29 +15,6 @@ class IlliadUserManager( object ):
     def __init__(self):
         pass
 
-    # def manage_illiad_user_check( self, usr_dct ):
-    #     """ Manager for illiad handling.
-    #         - hits the new illiad-api for the status (`blocked`, `registered`, etc)
-    #             # - if problem, prepares failure message as-is (creating return-dct)
-    #             - if new-user, runs manage_new_user() and creates proper success or failure return-dct
-    #             # - if neither problem or new-user, TODO -- incorporate the new update-status api call here
-    #         Called by delivery.views.process_request()...
-    #           # ...which, on any failure, will store the returned crafted error message to the session,
-    #           # ...and redirect to an error page. """
-    #     # log.debug( '(common_classes) - usr_dct, ```%s```' % pprint.pformat(usr_dct) )
-    #     log.debug( 'usr_dct, ``%s``' % pprint.pformat(usr_dct) )
-    #     illiad_status_dct = self.check_illiad_status( usr_dct['eppn'].split('@')[0] )
-    #     log.debug( 'illiad_status_dct, ``%s``' % illiad_status_dct )
-    #     if illiad_status_dct['response']['status_data']['blocked'] is True or illiad_status_dct['response']['status_data']['disavowed'] is True:
-    #         # return_dct = self.make_illiad_problem_message( usr_dct, title )
-    #         log.warning( 'blocked or disavowed status detected' )
-    #     elif illiad_status_dct['response']['status_data']['interpreted_new_user'] is True:
-    #         return_dct = self.manage_new_user( usr_dct )
-    #     else:
-    #         return_dct = { 'success': True }
-    #     log.debug( 'return_dct, ```%s```' % pprint.pformat(return_dct) )
-    #     return return_dct
-
     def manage_illiad_user_check( self, usr_dct ):
         """ Manager for illiad handling.
             - hits the new illiad-api for the status (`blocked`, `registered`, etc)
@@ -59,25 +36,23 @@ class IlliadUserManager( object ):
             else:
                 return_dct = { 'success': True }
             log.debug( 'return_dct, ```%s```' % pprint.pformat(return_dct) )
-            return return_dct
+            # return return_dct
         except:
             err = 'problem with ILLiad new-user check'
             log.exception( err )
         log.debug( f'err, ``{err}``' )
         return err
 
-
-
     def check_illiad_status( self, auth_id ):
         """ Hits our internal illiad-api for user's status (`blocked`, `registered`, etc).
             Called by manage_illiad_user_check() """
         rspns_dct = { 'response':
             {'status_data': {'blocked': None, 'disavowed': None}} }
-        url = '%s%s' % ( settings.ILLIAD_USER_API_URL_ROOT, 'check_user/' )
+        url = '%s%s' % ( settings.ILLIAD_API_URL_ROOT, 'check_user/' )
         params = { 'user': auth_id }
         try:
             header_dct = { 'user-agent': settings.ILLIAD_API_USER_AGENT }
-            r = requests.get( url, params=params, auth=(settings.ILLIAD_USER_API_BASIC_AUTH_USER, settings.ILLIAD_USER_API_BASIC_AUTH_PASSWORD), headers=header_dct, verify=True, timeout=10 )
+            r = requests.get( url, params=params, auth=(settings.ILLIAD_API_BASIC_AUTH_USER, settings.ILLIAD_API_BASIC_AUTH_PASSWORD), headers=header_dct, verify=True, timeout=10 )
             # rspns_dct = r.json()
             rspns_dct = json.loads( r.content )
             log.debug( 'status_code, `%s`; content-dct, ```%s```' % (r.status_code, pprint.pformat(rspns_dct)) )
@@ -129,7 +104,7 @@ class IlliadUserManager( object ):
             'phone': usr_dct['phone'],
             'department': usr_dct['department'] }
         success_check = False
-        url = '%s%s' % ( settings.ILLIAD_USER_API_URL_ROOT, 'create_user/' )
+        url = '%s%s' % ( settings.ILLIAD_API_URL_ROOT, 'create_user/' )
         log.debug( 'params, ```%s```; success_check, `%s`; url, ```%s```' % (pprint.pformat(params), success_check, url) )
         return ( params, success_check, url )
 
@@ -139,7 +114,7 @@ class IlliadUserManager( object ):
         username = usr_dct['eppn'].split('@')[0]
         brown_type = usr_dct['brown_type']
         log.debug( 'username, `%s`; brown_type, `%s`' % (username, brown_type) )
-        url = '%s%s' % ( settings.ILLIAD_USER_API_URL_ROOT, 'update_status/' )
+        url = '%s%s' % ( settings.ILLIAD_API_URL_ROOT, 'update_status/' )
         params = { 'auth_key': settings.ILLIAD_API_KEY, 'user':username, 'requested_status': brown_type }
         try:
             r = requests.post( url, data=params, verify=True, timeout=10 )
